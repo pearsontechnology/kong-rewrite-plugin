3965d63c6d938b48bd5ef334e49daeb2bd08955e
jdarling <jeremy.darling@gmail.com>
2017-04-12 09:38:04 -0500

    Remove commented old versions that will never be gone back to

--- schema.lua
+++ schema.lua
@@ -8,13 +8,13 @@ return {
         fields = {
           method = { type = "string", default = "" },
           request_method = { type = "string", default = "" },
-          request_querystring = { type = "string", default = "" },
-          request_text = { type = "string", default = "" },
-          request_json = { type = "string", default = "" },
-          request_headers = { type = "string", default = "" },
-          response_text = { type = "string", default = "" },
-          response_json = { type = "string", default = "" },
-          response_headers = { type = "string", default = "" },
+          request_querystring = { type = "string", default = "", multiline = true },
+          request_text = { type = "string", default = "", multiline = true },
+          request_json = { type = "string", default = "", multiline = true },
+          request_headers = { type = "string", default = "", multiline = true },
+          response_text = { type = "string", default = "", multiline = true },
+          response_json = { type = "string", default = "", multiline = true },
+          response_headers = { type = "string", default = "", multiline = true },
         }
       }
     }
@@ -51,48 +51,6 @@ return {
         }
       }
     }
-  --[[
-  fields = {
-    uri = {
-      type = "table",
-      schema = {
-        fields = {
-          route = { type = "string", default = "" },
-          method = { type = "string", default = "" },
-        }
-      }
-    },
-    request = {
-      type = "table",
-      schema = {
-        fields = {
-          method = { type = "string", default = "" },
-          querystring = { type = "string", default = "" },
-          text = { type = "string", default = "" },
-          json = { type = "string", default = "" },
-          headers = { type = "string", default = "" },
-        }
-      }
-    },
-    response = {
-      type = "table",
-      schema = {
-        fields = {
-          text = { type = "string", default = "" },
-          json = { type = "string", default = "" },
-          headers = { type = "string", default = "" },
-        }
-      }
-    },
-    --[[
-    rewrite_request_method = { type = "string", default = "" },
-    rewrite_request_querystring = { type = "string", default = "" },
-    rewrite_request_text = { type = "string", default = "" },
-    rewrite_request_json = { type = "string", default = "" },
-    rewrite_request_headers = { type = "string", default = "" },
-    rewrite_response_text = { type = "string", default = "" },
-    rewrite_response_json = { type = "string", default = "" },
-    rewrite_response_headers = { type = "string", default = "" },
     ]]
   }
 }
